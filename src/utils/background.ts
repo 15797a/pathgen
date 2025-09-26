e53c5e69355912152adb3c623de13d460346bcb7
halp <haelpf@gmail.com>
2025-09-26 09:21:12 -0400

    small upgrade for push back backgrounds

--- src/utils/background.ts
+++ src/utils/background.ts
@@ -2,6 +2,8 @@ import overUnder from "../assets/backgrounds/over-under.png";
 import overUnderSkills from "../assets/backgrounds/over-under-skills.png";
 import highStakes from "../assets/backgrounds/high-stakes.png";
 import highStakesSkills from "../assets/backgrounds/high-stakes-skills.png";
+import pushBack from "../assets/backgrounds/push-back.png";
+import pushBackSkills from "../assets/backgrounds/push-back-skills.png";
 import defaultBackground from "../assets/backgrounds/default.png";
 
 export type Background =
@@ -9,6 +11,8 @@ export type Background =
   | "over-under-skills"
   | "high-stakes"
   | "high-stakes-skills"
+	| "push-back"
+	| "push-back-skills"
   | "default";
 
 export const backgrounds: { [key in Background]: string } = {
@@ -16,5 +20,7 @@ export const backgrounds: { [key in Background]: string } = {
   "high-stakes-skills": highStakesSkills,
   "over-under": overUnder,
   "over-under-skills": overUnderSkills,
+	"push-back": pushBack,
+	"push-back-skills": pushBackSkills,
   default: defaultBackground,
 };
